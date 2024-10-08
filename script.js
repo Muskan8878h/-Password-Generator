8060d06183de8da914054b4e6d80b459ad19eff0
muskan <muskan8878h@gmail.com>
2024-10-08 16:10:11 +0530

    create a function which select random ablpabet number ofr symbol

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerSet = "abcdefghijklmnopqrstuvwxyz";
+const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerSet = "abcdefghijklmnopqrstuvwxyz"
 const numberSet = "1234567890"
-const symbolSet = "~!@#$%^&*()_+-=?/";
+const symbolSet = "~!@#$%^&*()_+/"
+
+const getRandomData = (dataSet) => {
+    return dataSet[Math.floor(Math.random() * dataSet.length)]
+}
